f6d9dcf7d708b69523605521ca9e105da570df13
zhoulinze123 <904238691@qq.com>
2026-09-16 15:16:53 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'runlin-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
